3ad1afb79eb72f164917c99bf21a0257bf04797a
Diego Oliveira <diegoliveiraa1@gmail.com>
2026-04-22 20:00:46 -0300

    feat: add JWT token generation script for admin user

---
+++ scripts/gen_token.go
@@ -0,0 +1,17 @@
+package main
+
+  import (
+        "fmt"
+        "time"
+        "github.com/golang-jwt/jwt/v5"
+  )
+
+  func main() {
+        claims := jwt.MapClaims{
+                "sub": "admin-postman",
+                "exp": time.Now().Add(24 * time.Hour).Unix(),
+        }
+        token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+        signed, _ := token.SignedString([]byte("change-me-in-production"))
+        fmt.Println(signed)
+  }
