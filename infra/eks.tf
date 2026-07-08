ed5dfb57f425d6277bc31d8d9145c213b5853c85
Yuri Padlipskas <yupadlipskas@gmail.com>
2026-07-08 16:32:54 -0300

    fix: eks v20 to v19

--- infra/eks.tf
+++ infra/eks.tf
@@ -1,12 +1,11 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  version = "~> 19.0"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.30"
+  cluster_version = "1.29"
 
-  cluster_endpoint_public_access           = true
-  enable_cluster_creator_admin_permissions = false
+  cluster_endpoint_public_access = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
