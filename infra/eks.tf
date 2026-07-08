1e8e2d7a0788c6e1d9515ba0eba42afc0165df12
Yuri Padlipskas <yupadlipskas@gmail.com>
2026-07-08 16:31:18 -0300

    fix: iam get role

--- infra/eks.tf
+++ infra/eks.tf
@@ -5,7 +5,8 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = "1.30"
 
-  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access           = true
+  enable_cluster_creator_admin_permissions = false
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
